3cb2843dae06e29b6ceeda43e9b189f6e16536ea
hendiksaputra <hendiksaputra04@gmail.com>
2022-12-13 14:52:58 +0800

    menambahkan view

---
+++ resources/views/additionaldata/action.blade.php
@@ -0,0 +1,70 @@
+<a class="btn btn-icon btn-primary" href="{{ url('additionaldatas/' . $additionaldatas->id . '/show') }}" data-toggle="modal" data-target="#modal-lg-{{ $additionaldatas->id }}"><i class="fas fa-pen-square"></i></a>
+
+
+<div class="modal fade text-left" id="modal-lg-{{ $additionaldatas->id }}">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title">Additional Data Employee</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form action="{{ url('additionaldatas/' . $additionaldatas->id) }}" method="POST">
+        <div class="modal-body">
+          <div class="card-body">
+            <div class="tab-content p-0">
+              <div class="form-group row">
+                <label class="col-sm-3 col-form-label">Employee Name</label>
+                <div class="col-sm-8">
+                  <input type="text" class="form-control" name="fullname" value="{{ $additionaldatas->fullname }}" readonly>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-sm-3 col-form-label">Cloth Size</label>
+                <div class="col-sm-8">
+                  <input type="text" class="form-control" name="cloth_size" value="{{ $additionaldatas->cloth_size }}" readonly>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-sm-3 col-form-label">Pants Size</label>
+                <div class="col-sm-8">
+                  <input type="text" class="form-control" name="pants_size" value="{{ $additionaldatas->pants_size }}" readonly>
+                  
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-sm-3 col-form-label">Shoes Size</label>
+                <div class="col-sm-8">
+                  <input type="text" class="form-control" name="shoes_size" value="{{ $additionaldatas->shoes_size }}" readonly>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-sm-3 col-form-label">Height</label>
+                <div class="col-sm-8">
+                  <input type="text" class="form-control" name="height" value="{{ $additionaldatas->height }}" readonly>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-sm-3 col-form-label">Weight</label>
+                <div class="col-sm-8">
+                  <input type="text" class="form-control" name="weight" value="{{ $additionaldatas->weight }}" readonly>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-sm-3 col-form-label">Glasses</label>
+                <div class="col-sm-8">
+                  <input type="text" class="form-control" name="glasses" value="{{ $additionaldatas->glasses }}" readonly>
+                </div>
+              </div>
+             
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer justify-content-between">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
---
+++ resources/views/license/action.blade.php
@@ -0,0 +1,51 @@
+<a class="btn btn-icon btn-primary" href="{{ url('licenses/' . $license->id . '/show') }}" data-toggle="modal" data-target="#modal-lg-{{ $license->id }}"><i class="fas fa-pen-square"></i></a>
+
+<div class="modal fade text-left" id="modal-lg-{{ $license->id }}">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title">Employee Driver Licensee</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form action="{{ url('licenses/' . $license->id) }}" method="POST">
+        <div class="modal-body">
+          <div class="card-body">
+            <div class="tab-content p-0">
+              <div class="form-group row">
+                <label class="col-sm-3 col-form-label">Employee Name</label>
+                <div class="col-sm-8">
+                  <input type="text" class="form-control" name="fullname" value="{{ $license->fullname }}" readonly>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-sm-3 col-form-label">Driver License No</label>
+                <div class="col-sm-8">
+                  <input type="text" class="form-control" name="driver_license_no" value="{{ $license->driver_license_no }}" readonly>
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-sm-3 col-form-label">Driver License Type</label>
+                <div class="col-sm-8">
+                  <input type="text" class="form-control" name="driver_license_type" value="{{ $license->driver_license_type }}" readonly>
+                  
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-sm-3 col-form-label">Driver License Exp</label>
+                <div class="col-sm-8">
+                  <input type="text" class="form-control" name="driver_license_exp" value="{{ $license->driver_license_exp }}" readonly>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer justify-content-between">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
